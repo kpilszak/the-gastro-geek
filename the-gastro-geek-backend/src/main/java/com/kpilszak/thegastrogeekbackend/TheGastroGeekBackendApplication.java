0b9fef616a8ab3277a6c8280b210b97776dd1948
kpilszak <klaudia.pilszak@gmail.com>
2024-06-25 21:20:01 -0400

    Initial commit with backend project

---
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/TheGastroGeekBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kpilszak.thegastrogeekbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGastroGeekBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheGastroGeekBackendApplication.class, args);
+    }
+
+}
---
+++ the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/TheGastroGeekBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpilszak.thegastrogeekbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGastroGeekBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
