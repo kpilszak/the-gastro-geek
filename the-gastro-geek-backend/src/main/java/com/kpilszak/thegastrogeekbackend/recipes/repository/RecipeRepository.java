e45594a0cd60cb379a69d0e8f59c11c84ac1dc82
kpilszak <klaudia.pilszak@gmail.com>
2024-07-27 19:43:26 -0400

    Add findAll to RecipeRepository

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/recipes/repository/RecipeRepository.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/recipes/repository/RecipeRepository.java
@@ -1,7 +1,11 @@
 package com.kpilszak.thegastrogeekbackend.recipes.repository;
 
 import com.kpilszak.thegastrogeekbackend.recipes.domain.Recipe;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+
+    Page<Recipe> findAll(Pageable pageable);
 }
--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/recipes/service/RecipeService.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/recipes/service/RecipeService.java
@@ -7,4 +7,5 @@ import org.springframework.data.domain.Pageable;
 public interface RecipeService {
 
     Page<Recipe> getAllRecipes(Pageable pageable);
+
 }
