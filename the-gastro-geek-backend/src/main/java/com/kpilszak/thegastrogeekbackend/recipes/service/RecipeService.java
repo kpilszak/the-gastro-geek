0dc574dadd2895b43a8a71fde4026fc847ebd740
kpilszak <klaudia.pilszak@gmail.com>
2024-07-26 21:36:14 -0400

    Add paging to getAllRecipes

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/recipes/service/RecipeService.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/recipes/service/RecipeService.java
@@ -1,10 +1,10 @@
 package com.kpilszak.thegastrogeekbackend.recipes.service;
 
 import com.kpilszak.thegastrogeekbackend.recipes.domain.Recipe;
-
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface RecipeService {
 
-    List<Recipe> getAllRecipes();
+    Page<Recipe> getAllRecipes(Pageable pageable);
 }
