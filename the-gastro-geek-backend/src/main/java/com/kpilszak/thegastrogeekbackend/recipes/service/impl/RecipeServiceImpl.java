5ba4f001e859ebbed2691b6afcebad852620e007
kpilszak <klaudia.pilszak@gmail.com>
2024-09-01 19:49:48 -0400

    Implement RecipeService as null pattern

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/recipes/service/impl/RecipeServiceImpl.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/recipes/service/impl/RecipeServiceImpl.java
@@ -1,7 +1,17 @@
 package com.kpilszak.thegastrogeekbackend.recipes.service.impl;
 
+import com.kpilszak.thegastrogeekbackend.recipes.dto.RecipeResponseDTO;
+import com.kpilszak.thegastrogeekbackend.recipes.service.RecipeService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
-public class RecipeServiceImpl {
+public class RecipeServiceImpl implements RecipeService {
+
+    @Override
+    public Page<RecipeResponseDTO> getAllRecipes(Pageable pageable) {
+        return null;
+    }
+
 }
