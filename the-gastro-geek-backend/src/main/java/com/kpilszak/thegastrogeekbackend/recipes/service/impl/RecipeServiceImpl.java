5442a9c8d4f7dc675baee6124764830daf2b73bd
kpilszak <klaudia.pilszak@gmail.com>
2024-09-04 19:52:37 -0400

    Inject RecipeRepository bean to RecipeServiceImpl

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/recipes/service/impl/RecipeServiceImpl.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/recipes/service/impl/RecipeServiceImpl.java
@@ -1,13 +1,18 @@
 package com.kpilszak.thegastrogeekbackend.recipes.service.impl;
 
 import com.kpilszak.thegastrogeekbackend.recipes.dto.RecipeResponseDTO;
+import com.kpilszak.thegastrogeekbackend.recipes.repository.RecipeRepository;
 import com.kpilszak.thegastrogeekbackend.recipes.service.RecipeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class RecipeServiceImpl implements RecipeService {
+
+    private final RecipeRepository recipeRepository;
 
     @Override
     public Page<RecipeResponseDTO> getAllRecipes(Pageable pageable) {
