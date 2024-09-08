537267af8664d81740c87ca40f0f33fe28a80488
kpilszak <klaudia.pilszak@gmail.com>
2024-09-07 20:01:25 -0400

    Add recipe from dto to domain mapping

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/mapper/RecipeMapper.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/mapper/RecipeMapper.java
@@ -1,7 +1,12 @@
 package com.kpilszak.thegastrogeekbackend.mapper;
 
+import com.kpilszak.thegastrogeekbackend.recipes.domain.Recipe;
+import com.kpilszak.thegastrogeekbackend.recipes.dto.RecipeRequestDTO;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface RecipeMapper {
+
+    Recipe toDomain(RecipeRequestDTO dto);
+
 }
