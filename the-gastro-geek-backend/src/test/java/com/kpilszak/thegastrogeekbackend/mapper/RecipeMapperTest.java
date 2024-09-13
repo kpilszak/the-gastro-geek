8d64f0ea15ded2afea5dbabe4f129cd37da5850b
kpilszak <klaudia.pilszak@gmail.com>
2024-09-12 21:18:30 -0400

    Prepare RecipeMapperTest methods skeleton

--- the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/mapper/RecipeMapperTest.java
+++ the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/mapper/RecipeMapperTest.java
@@ -1,14 +1,19 @@
 package com.kpilszak.thegastrogeekbackend.mapper;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class RecipeMapperTest {
 
     @Autowired
     private RecipeMapper recipeMapper;
+
+    @Test
+    void toDomain_shouldMapRequestDTOToDomain_whenGivenValidDTO() {}
+
+    @Test
+    void toDTO_shouldMapDomainToResponseDTO_whenGivenValidDomain() {}
 
 }
