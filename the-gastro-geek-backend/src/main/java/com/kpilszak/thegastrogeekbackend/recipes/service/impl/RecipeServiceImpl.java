1ddfef83d62df506c26f85171535ddbb4db80573
kpilszak <klaudia.pilszak@gmail.com>
2024-09-29 21:07:01 -0400

    fix: Create constructor manually in service impl

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/recipes/service/impl/RecipeServiceImpl.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/recipes/service/impl/RecipeServiceImpl.java
@@ -3,16 +3,21 @@ package com.kpilszak.thegastrogeekbackend.recipes.service.impl;
 import com.kpilszak.thegastrogeekbackend.recipes.dto.RecipeResponseDTO;
 import com.kpilszak.thegastrogeekbackend.recipes.repository.RecipeRepository;
 import com.kpilszak.thegastrogeekbackend.recipes.service.RecipeService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class RecipeServiceImpl implements RecipeService {
 
     private final RecipeRepository recipeRepository;
+
+    public RecipeServiceImpl(
+            RecipeRepository recipeRepository
+    ) {
+        this.recipeRepository = recipeRepository;
+    }
 
     @Override
     public Page<RecipeResponseDTO> getAllRecipes(Pageable pageable) {
