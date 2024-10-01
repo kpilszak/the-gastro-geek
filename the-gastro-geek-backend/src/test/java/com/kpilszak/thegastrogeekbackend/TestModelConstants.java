802fa1de00a707f0a980cea72f0f1f6570e2c2e1
kpilszak <klaudia.pilszak@gmail.com>
2024-09-30 21:07:40 -0400

    fix: Remove supplying in test

--- the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/TestModelConstants.java
+++ the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/TestModelConstants.java
@@ -4,11 +4,9 @@ import com.kpilszak.thegastrogeekbackend.recipes.domain.Recipe;
 import org.instancio.Instancio;
 import org.instancio.Model;
 
-import static org.instancio.Bindings.field;
-
 public class TestModelConstants {
 
     public static Model<Recipe> RECIPE_MODEL = Instancio.of(Recipe.class)
-            .supply(field(Recipe::getCalories), gen -> gen.ints().range(100, 1000))
+//            .supply(field(Recipe::getCalories), gen -> gen.ints().range(100, 1000))
             .toModel();
 }
