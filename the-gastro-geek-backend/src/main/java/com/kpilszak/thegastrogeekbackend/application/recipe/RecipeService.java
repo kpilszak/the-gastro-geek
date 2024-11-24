0125582e42e5af9c1c66c66124d211e90f841f0b
kpilszak <klaudia.pilszak@gmail.com>
2024-11-24 05:37:47 -0500

    fix: Return type in service

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/recipe/RecipeService.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/recipe/RecipeService.java
@@ -1,9 +1,9 @@
 package com.kpilszak.thegastrogeekbackend.application.recipe;
 
-import com.kpilszak.thegastrogeekbackend.infrastructure.recipe.persistence.entity.RecipeEntity;
+import com.kpilszak.thegastrogeekbackend.application.recipe.dto.RecipeResponseDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface RecipeService {
-    Page<RecipeEntity> getAllRecipes(Pageable pageable);
+    Page<RecipeResponseDTO> getAllRecipes(Pageable pageable);
 }
