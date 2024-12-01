0e53c33501b331693ab8c553a6fc0f178974bbb4
kpilszak <klaudia.pilszak@gmail.com>
2024-12-01 06:02:15 -0500

    fix: Mapper return types

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/mapper/RecipeMapper.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/mapper/RecipeMapper.java
@@ -1,13 +1,13 @@
 package com.kpilszak.thegastrogeekbackend.infrastructure.recipe.mapper;
 
-import com.kpilszak.thegastrogeekbackend.infrastructure.recipe.persistence.entity.RecipeEntity;
 import com.kpilszak.thegastrogeekbackend.application.recipe.dto.RecipeRequestDTO;
 import com.kpilszak.thegastrogeekbackend.application.recipe.dto.RecipeResponseDTO;
+import com.kpilszak.thegastrogeekbackend.domain.recipe.model.Recipe;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface RecipeMapper {
-    RecipeEntity toDomain(RecipeRequestDTO dto);
+    Recipe fromDTO(RecipeRequestDTO dto);
 
-    RecipeResponseDTO toDTO(RecipeEntity recipeEntity);
+    RecipeResponseDTO toDTO(Recipe domain);
 }
