150ced8280fb1866af04b29b9f3f2660a98f8123
kpilszak <klaudia.pilszak@gmail.com>
2024-12-05 06:04:43 -0500

    fix: RepositoryAdapter is spring component

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/persistence/adapter/RecipeRepositoryAdapter.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/persistence/adapter/RecipeRepositoryAdapter.java
@@ -1,4 +1,7 @@
 package com.kpilszak.thegastrogeekbackend.infrastructure.recipe.persistence.adapter;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class RecipeRepositoryAdapter {
 }
