636257616d9531aaf9b851a623d1604655cbd933
kpilszak <klaudia.pilszak@gmail.com>
2024-12-07 06:06:49 -0500

    fix: RepositoryAdapter implements domain repository

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/persistence/adapter/RecipeRepositoryAdapter.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/persistence/adapter/RecipeRepositoryAdapter.java
@@ -1,7 +1,15 @@
 package com.kpilszak.thegastrogeekbackend.infrastructure.recipe.persistence.adapter;
 
+import com.kpilszak.thegastrogeekbackend.domain.recipe.model.Recipe;
+import com.kpilszak.thegastrogeekbackend.domain.recipe.repository.RecipeRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 @Component
-public class RecipeRepositoryAdapter {
+public class RecipeRepositoryAdapter implements RecipeRepository {
+    @Override
+    public Page<Recipe> findAll(Pageable pageable) {
+        return null;
+    }
 }
