2707d12b323d8d96c88d95c779e6b005fc208f9a
kpilszak <klaudia.pilszak@gmail.com>
2024-12-08 06:09:32 -0500

    fix: Add @Table annotation to entity class

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/persistence/entity/RecipeEntity.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/persistence/entity/RecipeEntity.java
@@ -1,12 +1,10 @@
 package com.kpilszak.thegastrogeekbackend.infrastructure.recipe.persistence.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
+@Table(name = "recipes")
 @Getter
 @Setter
 @NoArgsConstructor
