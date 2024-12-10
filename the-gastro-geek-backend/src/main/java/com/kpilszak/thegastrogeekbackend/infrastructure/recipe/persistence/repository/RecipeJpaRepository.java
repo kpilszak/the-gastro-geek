078c2650bc755a51fa52266cda699f2c0cae6a51
kpilszak <klaudia.pilszak@gmail.com>
2024-12-10 07:08:25 -0500

    fix: Add @Repository annotation for JpaRepository interface

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/persistence/repository/RecipeJpaRepository.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/persistence/repository/RecipeJpaRepository.java
@@ -1,4 +1,7 @@
 package com.kpilszak.thegastrogeekbackend.infrastructure.recipe.persistence.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface RecipeJpaRepository {
 }
