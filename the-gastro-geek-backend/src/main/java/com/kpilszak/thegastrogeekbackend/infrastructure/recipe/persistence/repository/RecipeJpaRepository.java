d59f3535364a0649d748c97616689979e44c8dac
kpilszak <klaudia.pilszak@gmail.com>
2024-12-11 07:10:35 -0500

    fix: Extend JpaRepository

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/persistence/repository/RecipeJpaRepository.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/persistence/repository/RecipeJpaRepository.java
@@ -1,7 +1,9 @@
 package com.kpilszak.thegastrogeekbackend.infrastructure.recipe.persistence.repository;
 
+import com.kpilszak.thegastrogeekbackend.infrastructure.recipe.persistence.entity.RecipeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RecipeJpaRepository {
+public interface RecipeJpaRepository extends JpaRepository<RecipeEntity, Long> {
 }
