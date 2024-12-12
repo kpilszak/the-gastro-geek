74b165549fdc173060eab96fad0ada9fdf2ebd3b
kpilszak <klaudia.pilszak@gmail.com>
2024-12-12 07:13:34 -0500

    fix: Annotate controller

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/interfaces/RecipeController.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/interfaces/RecipeController.java
@@ -1,9 +1,13 @@
 package com.kpilszak.thegastrogeekbackend.interfaces;
 
+import com.kpilszak.thegastrogeekbackend.application.recipe.RecipeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/recipes")
+@RequiredArgsConstructor
 public class RecipeController {
+    private final RecipeService recipeService;
 }
