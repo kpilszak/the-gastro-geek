f3a011d749ffbdb2c5aa1317151356b0d64e3c59
kpilszak <klaudia.pilszak@gmail.com>
2024-12-14 07:23:50 -0500

    Add paging

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/interfaces/RecipeController.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/interfaces/RecipeController.java
@@ -4,6 +4,7 @@ import com.kpilszak.thegastrogeekbackend.application.recipe.RecipeService;
 import com.kpilszak.thegastrogeekbackend.application.recipe.dto.RecipeResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ public class RecipeController {
     private final RecipeService recipeService;
 
     @GetMapping
-    public Page<RecipeResponseDTO> getAllRecipes() {
+    public Page<RecipeResponseDTO> getAllRecipes(Pageable pageable) {
         return null;
     }
 }
