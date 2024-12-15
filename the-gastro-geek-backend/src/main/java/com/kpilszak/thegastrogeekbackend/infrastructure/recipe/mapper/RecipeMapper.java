33341eccec2dd3374341003935c109621e72ad16
kpilszak <klaudia.pilszak@gmail.com>
2024-12-15 07:31:31 -0500

    Extract page content

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/mapper/RecipeMapper.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/mapper/RecipeMapper.java
@@ -4,10 +4,20 @@ import com.kpilszak.thegastrogeekbackend.application.recipe.dto.RecipeRequestDTO
 import com.kpilszak.thegastrogeekbackend.application.recipe.dto.RecipeResponseDTO;
 import com.kpilszak.thegastrogeekbackend.domain.recipe.model.Recipe;
 import org.mapstruct.Mapper;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface RecipeMapper {
     Recipe fromDTO(RecipeRequestDTO dto);
 
     RecipeResponseDTO toDTO(Recipe domain);
+
+    default Page<RecipeResponseDTO> toDTOPage(Page<Recipe> page) {
+        List<RecipeResponseDTO> dtoList = page.getContent().stream()
+                .map(this::toDTO)
+                .toList();
+        return null;
+    }
 }
