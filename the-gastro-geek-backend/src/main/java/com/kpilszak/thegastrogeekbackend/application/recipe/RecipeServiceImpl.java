4c31418da690f0b83103f76a0540bcb6e2f94e6a
kpilszak <klaudia.pilszak@gmail.com>
2024-12-17 07:43:12 -0500

    fix: Constructor in extending service

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/recipe/RecipeServiceImpl.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/recipe/RecipeServiceImpl.java
@@ -2,15 +2,17 @@ package com.kpilszak.thegastrogeekbackend.application.recipe;
 
 import com.kpilszak.thegastrogeekbackend.domain.recipe.model.Recipe;
 import com.kpilszak.thegastrogeekbackend.domain.recipe.repository.RecipeRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class RecipeServiceImpl implements RecipeService {
     private final RecipeRepository recipeRepository;
+
+    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
 
     @Override
     public Page<Recipe> getAllRecipes(Pageable pageable) {
