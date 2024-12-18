16168d3c7b6f8f8d5c30c8d564f174f7d3e003cf
kpilszak <klaudia.pilszak@gmail.com>
2024-12-18 07:54:17 -0500

    fix: Constructor in controller

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/interfaces/RecipeController.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/interfaces/RecipeController.java
@@ -2,7 +2,6 @@ package com.kpilszak.thegastrogeekbackend.interfaces;
 
 import com.kpilszak.thegastrogeekbackend.application.recipe.RecipeService;
 import com.kpilszak.thegastrogeekbackend.application.recipe.dto.RecipeResponseDTO;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/recipes")
-@RequiredArgsConstructor
 public class RecipeController {
     private final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
 
     @GetMapping
     public Page<RecipeResponseDTO> getAllRecipes(Pageable pageable) {
