be63368f5ff2e7c3f840dadbdcc65c46a7793539
kpilszak <klaudia.pilszak@gmail.com>
2024-12-19 08:03:20 -0500

    Implement getAllPages in constructor

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/interfaces/RecipeController.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/interfaces/RecipeController.java
@@ -2,6 +2,7 @@ package com.kpilszak.thegastrogeekbackend.interfaces;
 
 import com.kpilszak.thegastrogeekbackend.application.recipe.RecipeService;
 import com.kpilszak.thegastrogeekbackend.application.recipe.dto.RecipeResponseDTO;
+import com.kpilszak.thegastrogeekbackend.infrastructure.recipe.mapper.RecipeMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/recipes")
 public class RecipeController {
     private final RecipeService recipeService;
+    private final RecipeMapper recipeMapper;
 
-    public RecipeController(RecipeService recipeService) {
+    public RecipeController(RecipeService recipeService, RecipeMapper recipeMapper) {
         this.recipeService = recipeService;
+        this.recipeMapper = recipeMapper;
     }
 
     @GetMapping
     public Page<RecipeResponseDTO> getAllRecipes(Pageable pageable) {
-        return null;
+        return recipeMapper.toDTOPage(recipeService.getAllRecipes(pageable));
     }
 }
