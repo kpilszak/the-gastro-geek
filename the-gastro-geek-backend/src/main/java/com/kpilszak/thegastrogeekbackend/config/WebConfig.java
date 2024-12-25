b8e9253d68b4a4424ebbd560b97da777f0dc337a
kpilszak <klaudia.pilszak@gmail.com>
2024-12-25 08:25:57 -0500

    Use api properties in WebConfig

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/config/WebConfig.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/config/WebConfig.java
@@ -1,11 +1,18 @@
 package com.kpilszak.thegastrogeekbackend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${api.prefix}")
+    private String apiPrefix;
+
+    @Value("${api.version}")
+    private String apiVersion;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
