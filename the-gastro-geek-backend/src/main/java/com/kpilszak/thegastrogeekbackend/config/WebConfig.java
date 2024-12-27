66ecd727b2bc922d4b1c91a1130dcb2bb55be9f4
kpilszak <klaudia.pilszak@gmail.com>
2024-12-27 08:32:58 -0500

    Create addInterceptors skeleton

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/config/WebConfig.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.kpilszak.thegastrogeekbackend.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -16,5 +17,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
     }
 }
