94140499c6f02672b654f251b30a2a8411f3f63e
kpilszak <klaudia.pilszak@gmail.com>
2024-12-29 08:37:59 -0500

    Create interceptor constructor

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/interceptor/ApiPrefixInterceptor.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/interceptor/ApiPrefixInterceptor.java
@@ -1,4 +1,9 @@
 package com.kpilszak.thegastrogeekbackend.infrastructure.interceptor;
 
 public class ApiPrefixInterceptor {
+    private final String apiPrefix;
+
+    public ApiPrefixInterceptor(String apiPrefix) {
+        this.apiPrefix = apiPrefix;
+    }
 }
