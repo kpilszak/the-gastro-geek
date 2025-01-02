dedb7e894727b8683010f75526ab5e9baa2123dc
kpilszak <klaudia.pilszak@gmail.com>
2025-01-02 08:42:51 -0500

    For ApiPrefixInterceptor implement HandlerInterceptor

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/interceptor/ApiPrefixInterceptor.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/interceptor/ApiPrefixInterceptor.java
@@ -1,6 +1,8 @@
 package com.kpilszak.thegastrogeekbackend.infrastructure.interceptor;
 
-public class ApiPrefixInterceptor {
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class ApiPrefixInterceptor implements HandlerInterceptor {
     private final String apiPrefix;
 
     public ApiPrefixInterceptor(String apiPrefix) {
