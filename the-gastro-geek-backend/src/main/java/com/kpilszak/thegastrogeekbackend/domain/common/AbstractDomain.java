bd82cf433b25d93f37aeefd9c8730b064da422a0
kpilszak <klaudia.pilszak@gmail.com>
2025-01-12 09:22:51 -0500

    Add AuditInfo data

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/domain/common/AbstractDomain.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/domain/common/AbstractDomain.java
@@ -1,4 +1,11 @@
 package com.kpilszak.thegastrogeekbackend.domain.common;
 
+import java.time.Instant;
+
 public abstract class AbstractDomain {
+    private String createdBy;
+    private String updatedBy;
+    private Instant createdDate;
+    private Instant updatedDate;
+    private Long version;
 }
--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/domain/common/AuditInfo.java
+++
@@ -1,4 +0,0 @@
-package com.kpilszak.thegastrogeekbackend.domain.common;
-
-public class AuditInfo {
-}
