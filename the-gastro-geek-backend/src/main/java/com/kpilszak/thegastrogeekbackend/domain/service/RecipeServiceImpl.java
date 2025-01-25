1cab1c00420b669243923dd6cc46a95e4531d555
kpilszak <klaudia.pilszak@gmail.com>
2025-01-25 15:40:13 -0500

    refactor: Domain service does not implement app service

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/domain/service/RecipeServiceImpl.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/domain/service/RecipeServiceImpl.java
@@ -1,6 +1,5 @@
 package com.kpilszak.thegastrogeekbackend.domain.service;
 
-import com.kpilszak.thegastrogeekbackend.application.RecipeService;
 import com.kpilszak.thegastrogeekbackend.domain.model.Recipe;
 import com.kpilszak.thegastrogeekbackend.domain.repository.RecipeRepository;
 import org.springframework.data.domain.Page;
@@ -8,14 +7,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
-public class RecipeServiceImpl implements RecipeService {
+public class RecipeServiceImpl {
     private final RecipeRepository recipeRepository;
 
     public RecipeServiceImpl(RecipeRepository recipeRepository) {
         this.recipeRepository = recipeRepository;
     }
 
-    @Override
     public Page<Recipe> getAllRecipes(Pageable pageable) {
         return recipeRepository.findAll(pageable);
     }
