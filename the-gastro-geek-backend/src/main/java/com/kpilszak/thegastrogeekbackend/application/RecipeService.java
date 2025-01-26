b24cd4828e7fc96ac153652429913605c63b233a
kpilszak <klaudia.pilszak@gmail.com>
2025-01-26 15:42:44 -0500

    refactor: App layer service to coordinate use cases, no business logic

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/RecipeService.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/RecipeService.java
@@ -1,9 +1,5 @@
 package com.kpilszak.thegastrogeekbackend.application;
 
-import com.kpilszak.thegastrogeekbackend.domain.model.Recipe;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface RecipeService {
-    Page<Recipe> getAllRecipes(Pageable pageable);
+public class RecipeService {
+//    Page<Recipe> getAllRecipes(Pageable pageable);
 }
