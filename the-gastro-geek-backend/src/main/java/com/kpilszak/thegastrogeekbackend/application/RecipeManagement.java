079ec728702a96b4df91c11c9b3d4dc123ba56ce
kpilszak <klaudia.pilszak@gmail.com>
2025-02-01 16:12:43 -0500

    refactor: Make Management app service class a Spring service

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/RecipeManagement.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/RecipeManagement.java
@@ -1,5 +1,13 @@
 package com.kpilszak.thegastrogeekbackend.application;
 
+import com.kpilszak.thegastrogeekbackend.domain.service.RecipeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class RecipeManagement {
+    private final RecipeService service;
+
 //    Page<Recipe> getAllRecipes(Pageable pageable);
 }
