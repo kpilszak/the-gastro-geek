e48a3d0f4226e06bff821c38758cafca0e716183
kpilszak <klaudia.pilszak@gmail.com>
2025-02-08 17:11:47 -0500

    refactor: Use lombok for constructor

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/interfaces/RecipeController.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/interfaces/RecipeController.java
@@ -3,6 +3,7 @@ package com.kpilszak.thegastrogeekbackend.interfaces;
 import com.kpilszak.thegastrogeekbackend.application.RecipeManagement;
 import com.kpilszak.thegastrogeekbackend.application.dto.RecipeResponseDTO;
 import com.kpilszak.thegastrogeekbackend.infrastructure.recipe.mapper.RecipeMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/recipes")
 public class RecipeController {
     private final RecipeManagement recipeManagement;
     private final RecipeMapper recipeMapper;
-
-    public RecipeController(RecipeManagement recipeManagement, RecipeMapper recipeMapper) {
-        this.recipeManagement = recipeManagement;
-        this.recipeMapper = recipeMapper;
-    }
 
     @GetMapping
     public Page<RecipeResponseDTO> getAllRecipes(Pageable pageable) {
