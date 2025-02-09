6902a8ad875c38c1b11f821c6602a7cb03fcece1
kpilszak <klaudia.pilszak@gmail.com>
2025-02-09 17:18:14 -0500

    refactor: Use lombok for constructor

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/domain/service/RecipeService.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/domain/service/RecipeService.java
@@ -2,17 +2,15 @@ package com.kpilszak.thegastrogeekbackend.domain.service;
 
 import com.kpilszak.thegastrogeekbackend.domain.model.Recipe;
 import com.kpilszak.thegastrogeekbackend.domain.repository.RecipeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class RecipeService {
     private final RecipeRepository recipeRepository;
-
-    public RecipeService(RecipeRepository recipeRepository) {
-        this.recipeRepository = recipeRepository;
-    }
 
     public Page<Recipe> getAllRecipes(Pageable pageable) {
         return recipeRepository.findAll(pageable);
