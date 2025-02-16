b82d9f02eda3ba2f66d2f61519eeb47f99dcd83e
kpilszak <klaudia.pilszak@gmail.com>
2025-02-15 19:17:04 -0500

    Add fromEntity mapping

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/mapper/RecipeMapper.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/mapper/RecipeMapper.java
@@ -3,6 +3,7 @@ package com.kpilszak.thegastrogeekbackend.infrastructure.recipe.mapper;
 import com.kpilszak.thegastrogeekbackend.application.dto.RecipeRequestDTO;
 import com.kpilszak.thegastrogeekbackend.application.dto.RecipeResponseDTO;
 import com.kpilszak.thegastrogeekbackend.domain.model.Recipe;
+import com.kpilszak.thegastrogeekbackend.infrastructure.recipe.persistence.entity.RecipeEntity;
 import org.mapstruct.Mapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -21,4 +22,6 @@ public interface RecipeMapper {
                 .toList();
         return new PageImpl<>(dtoList, page.getPageable(), page.getTotalElements());
     }
+
+    Recipe fromEntity(RecipeEntity entity);
 }
