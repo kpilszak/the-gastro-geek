edda10a59373783444d08c3687e37c6659fe9564
kpilszak <klaudia.pilszak@gmail.com>
2025-02-20 08:20:01 -0500

    Inject mapper into repository adapter

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/persistence/adapter/RecipeRepositoryAdapter.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/persistence/adapter/RecipeRepositoryAdapter.java
@@ -2,6 +2,7 @@ package com.kpilszak.thegastrogeekbackend.infrastructure.recipe.persistence.adap
 
 import com.kpilszak.thegastrogeekbackend.domain.model.Recipe;
 import com.kpilszak.thegastrogeekbackend.domain.repository.RecipeRepository;
+import com.kpilszak.thegastrogeekbackend.infrastructure.recipe.mapper.RecipeMapper;
 import com.kpilszak.thegastrogeekbackend.infrastructure.recipe.persistence.repository.RecipeJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class RecipeRepositoryAdapter implements RecipeRepository {
     private final RecipeJpaRepository repository;
+    private final RecipeMapper mapper;
 
     @Override
     public Page<Recipe> findAll(Pageable pageable) {
