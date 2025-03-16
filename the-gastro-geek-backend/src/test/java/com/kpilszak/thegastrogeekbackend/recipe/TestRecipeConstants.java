d3bb4486d90b38d9b4c1fc66ecb825cf26391612
kpilszak <klaudia.pilszak@gmail.com>
2025-03-16 05:08:17 -0400

    refactor: Add feature package for test constants

--- the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/recipe/TestRecipeConstants.java
+++ the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/recipe/TestRecipeConstants.java
@@ -1,10 +1,10 @@
-package com.kpilszak.thegastrogeekbackend;
+package com.kpilszak.thegastrogeekbackend.recipe;
 
 import com.kpilszak.thegastrogeekbackend.infrastructure.recipe.persistence.entity.recipe.RecipeEntity;
 import org.instancio.Instancio;
 import org.instancio.Model;
 
-public class TestModelConstants {
+public class TestRecipeConstants {
 
     public static Model<RecipeEntity> RECIPE_MODEL = Instancio.of(RecipeEntity.class)
 //            .supply(field(Recipe::getCalories), gen -> gen.ints().range(100, 1000))
