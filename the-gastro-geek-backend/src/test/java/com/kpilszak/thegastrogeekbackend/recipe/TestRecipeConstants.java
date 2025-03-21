23309e72c3caf10eab218fe5d099759d78e64a52
kpilszak <klaudia.pilszak@gmail.com>
2025-03-20 20:16:09 -0400

    refactor: Remove unnecessary white lines

--- the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/recipe/TestRecipeConstants.java
+++ the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/recipe/TestRecipeConstants.java
@@ -5,7 +5,6 @@ import org.instancio.Instancio;
 import org.instancio.Model;
 
 public class TestRecipeConstants {
-
     public static Model<RecipeEntity> RECIPE_MODEL = Instancio.of(RecipeEntity.class)
 //            .supply(field(Recipe::getCalories), gen -> gen.ints().range(100, 1000))
             .toModel();
