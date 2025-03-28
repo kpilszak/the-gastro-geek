1a11a016883d46ab74736c51f24c2634a0596baf
kpilszak <klaudia.pilszak@gmail.com>
2025-03-28 07:03:40 -0400

    refactor: Introduce AbstractDomainClass

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/domain/model/recipe/Recipe.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/domain/model/recipe/Recipe.java
@@ -1,5 +1,6 @@
 package com.kpilszak.thegastrogeekbackend.domain.model.recipe;
 
+import com.kpilszak.thegastrogeekbackend.domain.common.AbstractDomain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Recipe {
+public class Recipe extends AbstractDomain {
     private Long id;
     private String title;
     private String description;
