9d2d727091cbe821a3e18ac75644f4c36268e442
kpilszak <klaudia.pilszak@gmail.com>
2025-04-08 09:34:05 -0400

    Add SuperBuilder to AbstractDTO

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/dto/recipe/AbstractDTO.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/dto/recipe/AbstractDTO.java
@@ -1,7 +1,9 @@
 package com.kpilszak.thegastrogeekbackend.application.dto.recipe;
 
 import lombok.Data;
+import lombok.experimental.SuperBuilder;
 
 @Data
+@SuperBuilder
 public abstract class AbstractDTO {
 }
