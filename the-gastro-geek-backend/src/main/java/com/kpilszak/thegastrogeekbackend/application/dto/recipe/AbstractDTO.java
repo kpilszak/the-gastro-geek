f1adcf5a6f61e061ae2ec6b853886e4976bf980c
kpilszak <klaudia.pilszak@gmail.com>
2025-04-09 09:34:48 -0400

    Add AllArgsConstructor to AbstractDTO

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/dto/recipe/AbstractDTO.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/dto/recipe/AbstractDTO.java
@@ -1,9 +1,11 @@
 package com.kpilszak.thegastrogeekbackend.application.dto.recipe;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder
+@AllArgsConstructor
 public abstract class AbstractDTO {
 }
