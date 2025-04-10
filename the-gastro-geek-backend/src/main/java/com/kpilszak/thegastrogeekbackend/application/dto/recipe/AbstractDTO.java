4a37ead520d3889b3c6a6674614ce1dd0c5a5bf7
kpilszak <klaudia.pilszak@gmail.com>
2025-04-10 09:35:13 -0400

    Add NoArgsConstructor to AbstractDTO

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/dto/recipe/AbstractDTO.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/dto/recipe/AbstractDTO.java
@@ -2,10 +2,12 @@ package com.kpilszak.thegastrogeekbackend.application.dto.recipe;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder
 @AllArgsConstructor
+@NoArgsConstructor
 public abstract class AbstractDTO {
 }
