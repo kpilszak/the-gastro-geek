53a05c476831114337bdec0f248c8949d49dde68
kpilszak <klaudia.pilszak@gmail.com>
2025-04-11 09:36:14 -0400

    Add audit fields to AbstractDTO

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/dto/recipe/AbstractDTO.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/dto/recipe/AbstractDTO.java
@@ -5,9 +5,16 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import java.time.Instant;
+
 @Data
 @SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
 public abstract class AbstractDTO {
+    private String createdBy;
+    private String updatedBy;
+    private Instant createdDate;
+    private Instant updatedDate;
+    private Long version;
 }
