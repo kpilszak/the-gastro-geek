ebc50f701661d4187bb9f73f858016748021eb60
kpilszak <klaudia.pilszak@gmail.com>
2025-04-12 09:37:16 -0400

    Make AbstractDomain data

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/domain/model/AbstractDomain.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/domain/model/AbstractDomain.java
@@ -1,7 +1,10 @@
 package com.kpilszak.thegastrogeekbackend.domain.model;
 
+import lombok.Data;
+
 import java.time.Instant;
 
+@Data
 public abstract class AbstractDomain {
     private String createdBy;
     private String updatedBy;
