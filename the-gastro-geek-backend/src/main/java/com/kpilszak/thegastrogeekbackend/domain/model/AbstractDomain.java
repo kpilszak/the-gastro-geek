ed6a31496efb8ae8842761b57cb6b6dfdda8e305
kpilszak <klaudia.pilszak@gmail.com>
2025-04-13 09:38:48 -0400

    Add SuperBuilder and constructors to AbstractDomain

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/domain/model/AbstractDomain.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/domain/model/AbstractDomain.java
@@ -1,12 +1,16 @@
 package com.kpilszak.thegastrogeekbackend.domain.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import java.time.Instant;
 
 @Data
 @SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 public abstract class AbstractDomain {
     private String createdBy;
     private String updatedBy;
