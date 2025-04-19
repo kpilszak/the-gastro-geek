50b12baec6ed096c5993204079cfddaef7a43f10
kpilszak <klaudia.pilszak@gmail.com>
2025-04-19 15:18:49 -0400

    Add RECIPE_MODEL

--- the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/constants/model/TestRecipeConstants.java
+++ the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/constants/model/TestRecipeConstants.java
@@ -1,4 +1,9 @@
 package com.kpilszak.thegastrogeekbackend.constants.model;
 
+import com.kpilszak.thegastrogeekbackend.domain.model.recipe.Recipe;
+import org.instancio.Instancio;
+import org.instancio.Model;
+
 public class TestRecipeConstants {
+    public static final Model<Recipe> RECIPE_MODEL = Instancio.of(Recipe.class).toModel();
 }
