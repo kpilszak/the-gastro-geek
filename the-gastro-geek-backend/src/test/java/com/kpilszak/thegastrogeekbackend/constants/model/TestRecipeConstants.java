847522e10f4b9f4c63933df5612aa466bee3d223
kpilszak <klaudia.pilszak@gmail.com>
2025-04-24 15:23:53 -0400

    Add RECIPE_REQUEST_DTO

--- the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/constants/model/TestRecipeConstants.java
+++ the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/constants/model/TestRecipeConstants.java
@@ -1,9 +1,11 @@
 package com.kpilszak.thegastrogeekbackend.constants.model;
 
+import com.kpilszak.thegastrogeekbackend.application.dto.recipe.RecipeRequestDTO;
 import com.kpilszak.thegastrogeekbackend.domain.model.recipe.Recipe;
 import org.instancio.Instancio;
 import org.instancio.Model;
 
 public class TestRecipeConstants {
     public static final Model<Recipe> RECIPE_MODEL = Instancio.of(Recipe.class).toModel();
+    public static final Model<RecipeRequestDTO> RECIPE_REQUEST_DTO = Instancio.of(RecipeRequestDTO.class).toModel();
 }
