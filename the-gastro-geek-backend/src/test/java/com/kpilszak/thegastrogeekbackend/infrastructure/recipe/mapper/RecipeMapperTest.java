258b30e1a6dc51388f455d72ab1596259cd1bcb5
kpilszak <klaudia.pilszak@gmail.com>
2025-04-25 15:27:36 -0400

    Test fromDTO method

--- the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/mapper/RecipeMapperTest.java
+++ the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/mapper/RecipeMapperTest.java
@@ -1,8 +1,15 @@
 package com.kpilszak.thegastrogeekbackend.infrastructure.recipe.mapper;
 
+import com.kpilszak.thegastrogeekbackend.application.dto.recipe.RecipeRequestDTO;
+import com.kpilszak.thegastrogeekbackend.domain.model.recipe.Recipe;
+import org.instancio.Instancio;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static com.kpilszak.thegastrogeekbackend.constants.model.TestRecipeConstants.RECIPE_REQUEST_DTO;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 @SpringBootTest
 class RecipeMapperTest {
@@ -10,8 +17,18 @@ class RecipeMapperTest {
     private RecipeMapper mapper;
 
     @Test
-    void toDomain_shouldMapRequestDTOToDomain_whenGivenValidDTO() {}
+    void fromDTO_shouldMapToDomain_whenDTOUsed() {
+        var dto = Instancio.create(RECIPE_REQUEST_DTO);
 
-    @Test
-    void toDTO_shouldMapDomainToResponseDTO_whenGivenValidDomain() {}
+        var domain = mapper.fromDTO(dto);
+
+        assertFromDTORecipe(domain, dto);
+    }
+
+    private static void assertFromDTORecipe(Recipe domain, RecipeRequestDTO dto) {
+        assertThat(domain.getId(), is(dto.getId()));
+        assertThat(domain.getTitle(), is(dto.getTitle()));
+        assertThat(domain.getDescription(), is(dto.getDescription()));
+        assertThat(domain.getCalories(), is(dto.getCalories()));
+    }
 }
