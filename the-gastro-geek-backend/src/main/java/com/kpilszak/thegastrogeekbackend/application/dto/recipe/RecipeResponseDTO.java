7c9d60774b8da418474ffb7023183f8f0f2c7407
kpilszak <klaudia.pilszak@gmail.com>
2025-05-10 17:19:24 -0400

    Make SuperBuilder where necessary

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/dto/recipe/RecipeResponseDTO.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/dto/recipe/RecipeResponseDTO.java
@@ -2,14 +2,14 @@ package com.kpilszak.thegastrogeekbackend.application.dto.recipe;
 
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
+@SuperBuilder
 public class RecipeResponseDTO extends AbstractResponseDTO {
     private Long id;
     private String title;
