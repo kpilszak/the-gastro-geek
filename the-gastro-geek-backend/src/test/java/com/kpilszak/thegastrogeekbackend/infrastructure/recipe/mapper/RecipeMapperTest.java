9ad68316b34d7f43dabd8aee660819e16e2e857e
kpilszak <klaudia.pilszak@gmail.com>
2025-05-15 17:36:45 -0400

    Test toDTO method

--- the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/mapper/RecipeMapperTest.java
+++ the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/mapper/RecipeMapperTest.java
@@ -1,12 +1,14 @@
 package com.kpilszak.thegastrogeekbackend.infrastructure.recipe.mapper;
 
 import com.kpilszak.thegastrogeekbackend.application.dto.recipe.RecipeRequestDTO;
+import com.kpilszak.thegastrogeekbackend.application.dto.recipe.RecipeResponseDTO;
 import com.kpilszak.thegastrogeekbackend.domain.model.recipe.Recipe;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static com.kpilszak.thegastrogeekbackend.constants.model.TestRecipeConstants.RECIPE_MODEL;
 import static com.kpilszak.thegastrogeekbackend.constants.model.TestRecipeConstants.RECIPE_REQUEST_DTO;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -25,10 +27,26 @@ class RecipeMapperTest {
         assertRecipeFromDTO(domain, dto);
     }
 
+    @Test
+    void toDTO_shouldMapToDTO_whenDomainUsed() {
+        var domain = Instancio.create(RECIPE_MODEL);
+
+        var dto = mapper.toDTO(domain);
+
+        assertRecipeToDTO(dto, domain);
+    }
+
     private static void assertRecipeFromDTO(Recipe domain, RecipeRequestDTO dto) {
         assertThat(domain.getId(), is(dto.getId()));
         assertThat(domain.getTitle(), is(dto.getTitle()));
         assertThat(domain.getDescription(), is(dto.getDescription()));
         assertThat(domain.getCalories(), is(dto.getCalories()));
+    }
+
+    private static void assertRecipeToDTO(RecipeResponseDTO dto, Recipe domain) {
+        assertThat(dto.getId(), is(domain.getId()));
+        assertThat(dto.getTitle(), is(domain.getTitle()));
+        assertThat(dto.getDescription(), is(domain.getDescription()));
+        assertThat(dto.getCalories(), is(domain.getCalories()));
     }
 }
