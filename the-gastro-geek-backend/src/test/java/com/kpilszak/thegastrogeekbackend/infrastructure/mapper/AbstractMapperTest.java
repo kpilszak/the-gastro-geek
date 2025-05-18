a20bf7a77893076a21a852593a8aa99d358e6ce4
kpilszak <klaudia.pilszak@gmail.com>
2025-05-18 18:00:11 -0400

    Create AbstractMapperTest for audit fields

---
+++ the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/infrastructure/mapper/AbstractMapperTest.java
@@ -0,0 +1,17 @@
+package com.kpilszak.thegastrogeekbackend.infrastructure.mapper;
+
+import com.kpilszak.thegastrogeekbackend.application.dto.recipe.AbstractResponseDTO;
+import com.kpilszak.thegastrogeekbackend.domain.model.AbstractDomain;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class AbstractMapperTest {
+    protected void assertAuditToDTO(AbstractResponseDTO dto, AbstractDomain domain) {
+        assertThat(dto.getCreatedBy(), is(domain.getCreatedBy()));
+        assertThat(dto.getUpdatedBy(), is(domain.getUpdatedBy()));
+        assertThat(dto.getCreatedDate(), is(domain.getCreatedDate()));
+        assertThat(dto.getUpdatedDate(), is(domain.getUpdatedDate()));
+        assertThat(dto.getVersion(), is(domain.getVersion()));
+    }
+}
