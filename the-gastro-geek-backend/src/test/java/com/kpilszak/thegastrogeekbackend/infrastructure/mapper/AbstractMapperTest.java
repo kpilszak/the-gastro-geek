72286df073e4b20ba01e53de72c2b2edf720d237
kpilszak <klaudia.pilszak@gmail.com>
2025-05-29 21:54:48 -0400

    Write abstract page metadata asserts

--- the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/infrastructure/mapper/AbstractMapperTest.java
+++ the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/infrastructure/mapper/AbstractMapperTest.java
@@ -2,6 +2,7 @@ package com.kpilszak.thegastrogeekbackend.infrastructure.mapper;
 
 import com.kpilszak.thegastrogeekbackend.application.dto.recipe.AbstractResponseDTO;
 import com.kpilszak.thegastrogeekbackend.domain.model.AbstractDomain;
+import org.springframework.data.domain.Page;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -13,5 +14,12 @@ public class AbstractMapperTest {
         assertThat(dto.getCreatedDate(), is(domain.getCreatedDate()));
         assertThat(dto.getUpdatedDate(), is(domain.getUpdatedDate()));
         assertThat(dto.getVersion(), is(domain.getVersion()));
+    }
+
+    protected static void assertPageMetadataToDTO(Page<?> dtoPage, Page<?> domainPage) {
+        assertThat(dtoPage.getTotalElements(), is(domainPage.getTotalElements()));
+        assertThat(dtoPage.getNumber(), is(domainPage.getNumber()));
+        assertThat(dtoPage.getSize(), is(domainPage.getSize()));
+        assertThat(dtoPage.getContent().size(), is(domainPage.getContent().size()));
     }
 }
