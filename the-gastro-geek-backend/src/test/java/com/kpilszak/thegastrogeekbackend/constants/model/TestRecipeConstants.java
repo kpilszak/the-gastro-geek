ea5b515364ea4bed2c8ae540b2207652ec1eecb5
kpilszak <klaudia.pilszak@gmail.com>
2025-06-01 06:58:09 -0400

    Add RECIPE_ENTITY_MODEL

--- the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/constants/model/TestRecipeConstants.java
+++ the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/constants/model/TestRecipeConstants.java
@@ -2,10 +2,12 @@ package com.kpilszak.thegastrogeekbackend.constants.model;
 
 import com.kpilszak.thegastrogeekbackend.application.dto.recipe.RecipeRequestDTO;
 import com.kpilszak.thegastrogeekbackend.domain.model.recipe.Recipe;
+import com.kpilszak.thegastrogeekbackend.infrastructure.recipe.persistence.entity.recipe.RecipeEntity;
 import org.instancio.Instancio;
 import org.instancio.Model;
 
 public class TestRecipeConstants {
     public static final Model<Recipe> RECIPE_MODEL = Instancio.of(Recipe.class).toModel();
     public static final Model<RecipeRequestDTO> RECIPE_REQUEST_DTO_MODEL = Instancio.of(RecipeRequestDTO.class).toModel();
+    public static final Model<RecipeEntity> RECIPE_ENTITY_MODEL = Instancio.of(RecipeEntity.class).toModel();
 }
