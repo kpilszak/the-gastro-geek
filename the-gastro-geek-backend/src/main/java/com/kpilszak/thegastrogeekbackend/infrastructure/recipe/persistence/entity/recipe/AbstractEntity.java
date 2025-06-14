e7b8ff491ff80fc52f5e940658b7e92f418bdb59
kpilszak <klaudia.pilszak@gmail.com>
2025-06-14 18:12:20 -0400

    Add more audit fields in AbstractEntity

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/persistence/entity/recipe/AbstractEntity.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/recipe/persistence/entity/recipe/AbstractEntity.java
@@ -4,8 +4,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Version;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.*;
+
+import java.time.Instant;
 
 @MappedSuperclass
 @Getter
@@ -20,4 +24,19 @@ public abstract class AbstractEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
+
+    @CreatedBy
+    private String createdBy;
+
+    @CreatedDate
+    private Instant createdDate;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    private Instant lastModifiedDate;
+
+    @Version
+    private Long version;
 }
