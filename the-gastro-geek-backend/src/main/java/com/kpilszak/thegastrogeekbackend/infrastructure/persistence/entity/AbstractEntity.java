0539bf3a8078d8944e317250545ecb0c00ad2514
kpilszak <klaudia.pilszak@gmail.com>
2025-06-21 09:59:39 -0400

    Move AbstractEntity to general package

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/persistence/entity/AbstractEntity.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/persistence/entity/AbstractEntity.java
@@ -1,4 +1,4 @@
-package com.kpilszak.thegastrogeekbackend.infrastructure.persistence.entity.recipe;
+package com.kpilszak.thegastrogeekbackend.infrastructure.persistence.entity;
 
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
