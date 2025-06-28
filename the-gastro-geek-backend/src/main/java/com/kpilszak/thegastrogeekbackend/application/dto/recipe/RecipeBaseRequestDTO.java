231fbb42a8cf2499cd8738304f9382086c47b7e0
kpilszak <klaudia.pilszak@gmail.com>
2025-06-28 14:11:55 -0400

    Add lombok's ToString

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/dto/recipe/RecipeBaseRequestDTO.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/dto/recipe/RecipeBaseRequestDTO.java
@@ -4,12 +4,14 @@ package com.kpilszak.thegastrogeekbackend.application.dto.recipe;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @SuperBuilder
+@ToString
 public class RecipeBaseRequestDTO {
     private String title;
     private String description;
