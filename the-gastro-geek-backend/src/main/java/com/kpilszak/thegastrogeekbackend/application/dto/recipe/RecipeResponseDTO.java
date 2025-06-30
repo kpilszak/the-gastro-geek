0f1bbdb85ddb3da6f863a5f73fcf8e44705c556c
kpilszak <klaudia.pilszak@gmail.com>
2025-06-30 14:13:59 -0400

    Add ToString to dto

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/dto/recipe/RecipeResponseDTO.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/dto/recipe/RecipeResponseDTO.java
@@ -5,12 +5,14 @@ import com.kpilszak.thegastrogeekbackend.application.dto.AbstractResponseDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @SuperBuilder
+@ToString(callSuper = true)
 public class RecipeResponseDTO extends AbstractResponseDTO {
     private String title;
     private String description;
