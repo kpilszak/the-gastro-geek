3af489eddac2fb9f62ed103e4d969b7ececca920
kpilszak <klaudia.pilszak@gmail.com>
2025-07-05 14:43:53 -0400

    Adjust order in test asserts

--- the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/infrastructure/mapper/AbstractMapperTest.java
+++ the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/infrastructure/mapper/AbstractMapperTest.java
@@ -10,8 +10,8 @@ import static org.hamcrest.Matchers.is;
 public class AbstractMapperTest {
     protected static void assertAuditToDTO(AbstractResponseDTO dto, AbstractDomain domain) {
         assertThat(dto.getCreatedBy(), is(domain.getCreatedBy()));
-        assertThat(dto.getUpdatedBy(), is(domain.getLastUpdatedBy()));
         assertThat(dto.getCreatedDate(), is(domain.getCreatedDate()));
+        assertThat(dto.getUpdatedBy(), is(domain.getLastUpdatedBy()));
         assertThat(dto.getUpdatedDate(), is(domain.getLastUpdatedDate()));
         assertThat(dto.getVersion(), is(domain.getVersion()));
     }
