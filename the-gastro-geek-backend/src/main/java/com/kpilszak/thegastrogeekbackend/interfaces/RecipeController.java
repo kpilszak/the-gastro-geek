35c4c095bea5a09f024ae86d0cc4cdce0d22ca45
kpilszak <klaudia.pilszak@gmail.com>
2025-09-05 07:00:17 -0400

    Add create recipe skeleton endpoint

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/interfaces/RecipeController.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/interfaces/RecipeController.java
@@ -1,21 +1,26 @@
 package com.kpilszak.thegastrogeekbackend.interfaces;
 
 import com.kpilszak.thegastrogeekbackend.application.RecipeManagement;
+import com.kpilszak.thegastrogeekbackend.application.dto.recipe.RecipeRequestDTO;
 import com.kpilszak.thegastrogeekbackend.application.dto.recipe.RecipeResponseDTO;
 import com.kpilszak.thegastrogeekbackend.infrastructure.mapper.RecipeMapper;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/recipes")
+@Validated
 @RequiredArgsConstructor
 public class RecipeController {
     private final RecipeManagement management;
     private final RecipeMapper mapper;
+
+    @PostMapping
+    public RecipeResponseDTO create(@Valid @RequestBody RecipeRequestDTO requestDTO) { return null; }
 
     @GetMapping
     public Page<RecipeResponseDTO> getAll(Pageable pageable) {
