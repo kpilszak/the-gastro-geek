39b39839e3fe7498cedd668138eecc48adeec946
kpilszak <klaudia.pilszak@gmail.com>
2025-09-06 07:01:22 -0400

    Add recipe controller test skeleton

---
+++ the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/interfaces/RecipeControllerTest.java
@@ -0,0 +1,10 @@
+package com.kpilszak.thegastrogeekbackend.interfaces;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(RecipeController.class)
+class RecipeControllerTest {
+
+}
