439c8aa40b94362a8aaec5b3e925adbb4edd29fa
kpilszak <klaudia.pilszak@gmail.com>
2025-09-11 09:12:43 -0400

    Mock controller test beans

--- the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/interfaces/RecipeControllerTest.java
+++ the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/interfaces/RecipeControllerTest.java
@@ -1,10 +1,15 @@
 package com.kpilszak.thegastrogeekbackend.interfaces;
 
+import com.kpilszak.thegastrogeekbackend.application.RecipeManagement;
+import com.kpilszak.thegastrogeekbackend.infrastructure.mapper.RecipeMapper;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @WebMvcTest(RecipeController.class)
 class RecipeControllerTest {
+    @MockitoBean
+    private RecipeManagement management;
 
+    @MockitoBean
+    private RecipeMapper mapper;
 }
