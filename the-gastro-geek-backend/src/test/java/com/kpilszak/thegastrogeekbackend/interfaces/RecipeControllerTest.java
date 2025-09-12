59b8ad7621a63d1104f369d9c7d40c35ca7b0a3d
kpilszak <klaudia.pilszak@gmail.com>
2025-09-12 08:22:16 -0400

    Add necessary controller test autowired beans

--- the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/interfaces/RecipeControllerTest.java
+++ the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/interfaces/RecipeControllerTest.java
@@ -1,9 +1,12 @@
 package com.kpilszak.thegastrogeekbackend.interfaces;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kpilszak.thegastrogeekbackend.application.RecipeManagement;
 import com.kpilszak.thegastrogeekbackend.infrastructure.mapper.RecipeMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(RecipeController.class)
 class RecipeControllerTest {
@@ -12,4 +15,10 @@ class RecipeControllerTest {
 
     @MockitoBean
     private RecipeMapper mapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 }
