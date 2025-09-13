39a1cae5dd0d23b0b6cff271da1631f09f4040a5
kpilszak <klaudia.pilszak@gmail.com>
2025-09-13 15:53:12 -0400

    Prepare test method skeleton

--- the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/interfaces/RecipeControllerTest.java
+++ the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/interfaces/RecipeControllerTest.java
@@ -3,6 +3,7 @@ package com.kpilszak.thegastrogeekbackend.interfaces;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kpilszak.thegastrogeekbackend.application.RecipeManagement;
 import com.kpilszak.thegastrogeekbackend.infrastructure.mapper.RecipeMapper;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -21,4 +22,7 @@ class RecipeControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Test
+    void create_returnsCreated() {}
 }
