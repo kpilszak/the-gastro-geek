890677a99f07b3b53ed108271f9b9a417832ce9f
kpilszak <klaudia.pilszak@gmail.com>
2025-09-21 09:14:56 -0400

    Add pageable object to test

--- the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/interfaces/RecipeControllerTest.java
+++ the-gastro-geek-backend/src/test/java/com/kpilszak/thegastrogeekbackend/interfaces/RecipeControllerTest.java
@@ -6,6 +6,7 @@ import com.kpilszak.thegastrogeekbackend.infrastructure.mapper.RecipeMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -27,5 +28,7 @@ class RecipeControllerTest {
     void create_returnsCreated_whenCalled() {}
 
     @Test
-    void getAll_returnsPaged_whenCalled() {}
+    void getAll_returnsPaged_whenCalled() {
+        var pageable = PageRequest.of(0, 10);
+    }
 }
