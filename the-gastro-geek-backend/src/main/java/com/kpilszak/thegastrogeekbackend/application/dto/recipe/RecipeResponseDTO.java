50d96230d059bf8393ed803351be73f5d4532d84
kpilszak <klaudia.pilszak@gmail.com>
2025-10-16 05:52:14 -0400

    Fix annotations in RecipeResponseDTO

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/dto/recipe/RecipeResponseDTO.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/dto/recipe/RecipeResponseDTO.java
@@ -2,19 +2,16 @@ package com.kpilszak.thegastrogeekbackend.application.dto.recipe;
 
 
 import com.kpilszak.thegastrogeekbackend.application.dto.AbstractResponseDTO;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 @Data
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
-@SuperBuilder
-@ToString(callSuper = true, onlyExplicitlyIncluded = true)
 public class RecipeResponseDTO extends AbstractResponseDTO {
-    @ToString.Include
     private String title;
 
     private String description;
