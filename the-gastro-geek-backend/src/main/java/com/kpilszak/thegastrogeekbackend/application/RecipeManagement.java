d98f9dcc8fcb00020c752606106e75bc515d2972
kpilszak <klaudia.pilszak@gmail.com>
2025-10-26 06:38:14 -0400

    Remove getAll from management

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/RecipeManagement.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/RecipeManagement.java
@@ -1,18 +1,11 @@
 package com.kpilszak.thegastrogeekbackend.application;
 
-import com.kpilszak.thegastrogeekbackend.domain.model.recipe.Recipe;
 import com.kpilszak.thegastrogeekbackend.domain.service.RecipeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class RecipeManagement {
     private final RecipeService service;
-
-    public Page<Recipe> getAll(Pageable pageable) {
-        return service.getAll(pageable);
-    }
 }
