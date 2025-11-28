65cdd8637c2966312ff3cc7ed301e36bacb070b6
kpilszak <klaudia.pilszak@gmail.com>
2025-11-27 20:43:58 -0500

    Fix return type for findAllWithQuery

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/domain/repository/RecipeRepository.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/domain/repository/RecipeRepository.java
@@ -1,9 +1,10 @@
 package com.kpilszak.thegastrogeekbackend.domain.repository;
 
 import com.kpilszak.thegastrogeekbackend.domain.model.recipe.Recipe;
+import com.kpilszak.thegastrogeekbackend.domain.model.recipe.RecipeQueryResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface RecipeRepository {
-    Page<Recipe> findAllWithQuery(String query, Pageable pageable);
+    RecipeQueryResponse findAllWithQuery(String query, Pageable pageable);
 }
--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/persistence/adapter/RecipeRepositoryAdapter.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/persistence/adapter/RecipeRepositoryAdapter.java
@@ -1,12 +1,10 @@
 package com.kpilszak.thegastrogeekbackend.infrastructure.persistence.adapter;
 
-import com.kpilszak.thegastrogeekbackend.domain.model.recipe.Recipe;
 import com.kpilszak.thegastrogeekbackend.domain.model.recipe.RecipeQueryResponse;
 import com.kpilszak.thegastrogeekbackend.domain.repository.RecipeRepository;
 import com.kpilszak.thegastrogeekbackend.infrastructure.mapper.RecipeMapper;
 import com.kpilszak.thegastrogeekbackend.infrastructure.persistence.repository.RecipeJpaRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
