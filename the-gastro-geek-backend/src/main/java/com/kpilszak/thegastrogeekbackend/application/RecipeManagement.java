2dd06978d0e3f2a1d0bea50461ee17e1df34d64d
kpilszak <klaudia.pilszak@gmail.com>
2025-12-05 20:51:25 -0500

    Add getPagedWithQuery for management

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/RecipeManagement.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/application/RecipeManagement.java
@@ -1,11 +1,17 @@
 package com.kpilszak.thegastrogeekbackend.application;
 
+import com.kpilszak.thegastrogeekbackend.domain.model.recipe.RecipeQueryResponse;
 import com.kpilszak.thegastrogeekbackend.domain.service.RecipeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class RecipeManagement {
     private final RecipeService service;
+
+    public RecipeQueryResponse getPagedWithQuery(String query, Pageable pageable) {
+        return service.getPagedWithQuery(query, pageable);
+    }
 }
