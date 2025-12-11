1e98d9b8bb03285da3c2a9696d2c56abd654faff
kpilszak <klaudia.pilszak@gmail.com>
2025-12-11 17:18:45 -0500

    Add missing @Repository annotation

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/domain/repository/RecipeRepository.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/domain/repository/RecipeRepository.java
@@ -2,7 +2,9 @@ package com.kpilszak.thegastrogeekbackend.domain.repository;
 
 import com.kpilszak.thegastrogeekbackend.domain.model.recipe.RecipeQueryResponse;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RecipeRepository {
     RecipeQueryResponse findAllWithQuery(String query, Pageable pageable);
 }
