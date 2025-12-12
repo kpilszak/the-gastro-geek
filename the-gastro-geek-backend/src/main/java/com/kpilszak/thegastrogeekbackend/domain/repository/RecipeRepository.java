89313cc4b2cb2af2de2fdf0d3feed8a02f8c1d2c
kpilszak <klaudia.pilszak@gmail.com>
2025-12-12 17:21:16 -0500

    Change from Pageable -> PagingRequest

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/domain/repository/RecipeRepository.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/domain/repository/RecipeRepository.java
@@ -1,10 +1,10 @@
 package com.kpilszak.thegastrogeekbackend.domain.repository;
 
+import com.kpilszak.thegastrogeekbackend.domain.model.PagingRequest;
 import com.kpilszak.thegastrogeekbackend.domain.model.recipe.RecipeQueryResponse;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecipeRepository {
-    RecipeQueryResponse findAllWithQuery(String query, Pageable pageable);
+    RecipeQueryResponse findAllWithQuery(String query, PagingRequest pagingRequest);
 }
