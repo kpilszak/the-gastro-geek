da7a7fb9271d445b0e3aaf527db981c75eb1d7f2
kpilszak <klaudia.pilszak@gmail.com>
2025-12-20 19:32:41 -0500

    Use jpa repository in adapter

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/persistence/adapter/AbstractRepositoryAdapter.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/persistence/adapter/AbstractRepositoryAdapter.java
@@ -1,4 +1,13 @@
 package com.kpilszak.thegastrogeekbackend.infrastructure.persistence.adapter;
 
-public abstract class AbstractRepositoryAdapter {
+import com.kpilszak.thegastrogeekbackend.infrastructure.persistence.repository.BaseJpaRepository;
+
+public abstract class AbstractRepositoryAdapter<D, E, EI> {
+    protected final BaseJpaRepository<E, EI> repository;
+
+    protected AbstractRepositoryAdapter(
+            BaseJpaRepository<E, EI> repository
+    ) {
+        this.repository = repository;
+    }
 }
