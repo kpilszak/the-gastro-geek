24330e625c5f04dbc2f2b702c151cc4bc9667e11
kpilszak <klaudia.pilszak@gmail.com>
2026-01-04 15:46:17 -0500

    Extend BaseJpaRepository

--- the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/persistence/repository/RecipeJpaRepository.java
+++ the-gastro-geek-backend/src/main/java/com/kpilszak/thegastrogeekbackend/infrastructure/persistence/repository/RecipeJpaRepository.java
@@ -1,9 +1,8 @@
 package com.kpilszak.thegastrogeekbackend.infrastructure.persistence.repository;
 
 import com.kpilszak.thegastrogeekbackend.infrastructure.persistence.entity.recipe.RecipeEntity;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RecipeJpaRepository extends PagingAndSortingRepository<RecipeEntity, Long> {
+public interface RecipeJpaRepository extends BaseJpaRepository<RecipeEntity, Long> {
 }
